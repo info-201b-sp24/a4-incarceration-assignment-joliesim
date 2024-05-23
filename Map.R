371fe41978b4d16de26369a65bee2d8b0997cb8a
joliesim <jsim19@uw.edu>
2024-05-22 17:18:48 -0700

    Completed code for the map visualization. This map shows the distribution of black male prison population amongst WA state counties.

--- Map.R
+++ Map.R
@@ -1,9 +1,6 @@
 library(ggplot2)
 library(dplyr)
-library(stringr)
-library(tidyverse)
-library(maps)
-install.packages("tigris")
+
 data <- read.csv("https://raw.githubusercontent.com/melaniewalsh/Neat-Datasets/main/us-prison-pop.csv")
 prison_data <- na.omit(data)
 
@@ -17,15 +14,26 @@ max_black_male_pop <- wa_pops %>%
   group_by(county_name) %>%
   summarize(max_pop = max(black_male_prison_pop))
 
-library(tigris)
+updated_data <- max_black_male_pop %>%
+  mutate(county = tolower(county_name)) %>%
+  mutate(county_official = gsub(" county", "", county))
 
-wa_counties_data <- left_join(wa_counties, max_black_male_pop, by = c("NAME" = "county_name"))
+wa_shape <- map_data('county', 'washington')
 
-ggplot() +
-  geom_sf(data = wa_counties_data, aes(fill = max_pop)) +
-  scale_fill_gradient(name = "Max Black Male Prison Population",
-                      low = "lightblue", high = "darkblue", na.value = "grey",
-                      guide = "legend") +
-  labs(title = "Max Black Male Prison Population by County in Washington State") +
-  theme_minimal()
+state_shape <- wa_shape %>%
+  left_join(updated_data, by = c("subregion" = "county_official"))
 
+ggplot(state_shape) +
+  geom_polygon(
+    mapping = aes(x = long, y = lat, group = group, fill = max_pop), 
+    color = "white",
+    linewidth = .1
+  ) +
+  coord_map() +
+  scale_fill_continuous(low = "blue", high = "darkblue", na.value = "lightblue") +
+  labs(fill = "Black Male Prison Population",
+       title = "Distribution of Black Male Prison Population in Washington State Counties As of 2018") +
+  theme(axis.title = element_blank(), 
+        axis.text = element_blank(), 
+        axis.ticks = element_blank())
+         
